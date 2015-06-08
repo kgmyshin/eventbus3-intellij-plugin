995a67137c8c35b3b0cb0aad1b9a7834db577127
kgmyshin <kgmyshin82@gmail.com>
2015-06-08 21:50:01 +0900

    [add] eventbus3 plugin

---
+++ src/com/kgmyshin/ideaplugin/eventbus3/EventBus3LineMarkerProvider.java
@@ -0,0 +1,83 @@
+package com.kgmyshin.ideaplugin.eventbus3;
+
+import com.intellij.codeHighlighting.Pass;
+import com.intellij.codeInsight.daemon.GutterIconNavigationHandler;
+import com.intellij.codeInsight.daemon.LineMarkerInfo;
+import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.openapi.editor.markup.GutterIconRenderer;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.psi.*;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.util.PsiUtilBase;
+import com.intellij.ui.awt.RelativePoint;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Created by kgmyshin on 15/06/08.
+ */
+public class EventBus3LineMarkerProvider implements LineMarkerProvider {
+
+    public static final Icon ICON = IconLoader.getIcon("/icons/icon.png");
+
+    public static final int MAX_USAGES = 100;
+
+    private static GutterIconNavigationHandler<PsiElement> SHOW_SENDERS =
+            new GutterIconNavigationHandler<PsiElement>() {
+                @Override
+                public void navigate(MouseEvent e, PsiElement psiElement) {
+                    if (psiElement instanceof PsiMethod) {
+                        Project project = psiElement.getProject();
+                        JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(project);
+                        PsiClass eventBusClass = javaPsiFacade.findClass("de.greenrobot.event.EventBus", GlobalSearchScope.allScope(project));
+                        PsiMethod postMethod = eventBusClass.findMethodsByName("post", false)[0];
+                        PsiMethod method = (PsiMethod) psiElement;
+                        PsiClass eventClass = ((PsiClassType) method.getParameterList().getParameters()[0].getTypeElement().getType()).resolve();
+                        new ShowUsagesAction(new SenderFilter(eventClass)).startFindUsages(postMethod, new RelativePoint(e), PsiUtilBase.findEditor(psiElement), MAX_USAGES);
+                    }
+                }
+            };
+
+    private static GutterIconNavigationHandler<PsiElement> SHOW_RECEIVERS =
+            new GutterIconNavigationHandler<PsiElement>() {
+                @Override
+                public void navigate(MouseEvent e, PsiElement psiElement) {
+                    if (psiElement instanceof PsiMethodCallExpression) {
+                        PsiMethodCallExpression expression = (PsiMethodCallExpression) psiElement;
+                        PsiType[] expressionTypes = expression.getArgumentList().getExpressionTypes();
+                        if (expressionTypes.length > 0) {
+                            PsiClass eventClass = PsiUtils.getClass(expressionTypes[0]);
+                            if (eventClass != null) {
+                                new ShowUsagesAction(new ReceiverFilter()).startFindUsages(eventClass, new RelativePoint(e), PsiUtilBase.findEditor(psiElement), MAX_USAGES);
+                            }
+                        }
+                    }
+                }
+            };
+
+    @Nullable
+    @Override
+    public LineMarkerInfo getLineMarkerInfo(@NotNull PsiElement psiElement) {
+        if (PsiUtils.isEventBusPost(psiElement)) {
+            return new LineMarkerInfo<PsiElement>(psiElement, psiElement.getTextRange(), ICON,
+                    Pass.UPDATE_ALL, null, SHOW_RECEIVERS,
+                    GutterIconRenderer.Alignment.LEFT);
+        } else if (PsiUtils.isEventBusReceiver(psiElement)) {
+            return new LineMarkerInfo<PsiElement>(psiElement, psiElement.getTextRange(), ICON,
+                    Pass.UPDATE_ALL, null, SHOW_SENDERS,
+                    GutterIconRenderer.Alignment.LEFT);
+        }
+        return null;
+    }
+
+
+    @Override
+    public void collectSlowLineMarkers(@NotNull List<PsiElement> list, @NotNull Collection<LineMarkerInfo> collection) {
+    }
+}
---
+++ src/com/kgmyshin/ideaplugin/eventbus3/PingEDT.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.kgmyshin.ideaplugin.eventbus3;
+
+import com.intellij.openapi.util.Condition;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by kgmyshin on 2015/06/07.
+ */
+public class PingEDT {
+    private final String myName;
+    private final Runnable pingAction;
+    private volatile boolean stopped;
+    private volatile boolean pinged;
+    private final Condition<?> myShutUpCondition;
+    private final int myMaxUnitOfWorkThresholdMs; //-1 means indefinite
+
+    private final AtomicBoolean invokeLaterScheduled = new AtomicBoolean();
+    private final Runnable myUpdateRunnable = new Runnable() {
+        @Override
+        public void run() {
+            boolean b = invokeLaterScheduled.compareAndSet(true, false);
+            assert b;
+            if (stopped || myShutUpCondition.value(null)) {
+                stop();
+                return;
+            }
+            long start = System.currentTimeMillis();
+            int processed = 0;
+            while (true) {
+                if (processNext()) {
+                    processed++;
+                }
+                else {
+                    break;
+                }
+                long finish = System.currentTimeMillis();
+                if (myMaxUnitOfWorkThresholdMs != -1 && finish - start > myMaxUnitOfWorkThresholdMs) break;
+            }
+            if (!isEmpty()) {
+                scheduleUpdate();
+            }
+        }
+    };
+
+    public PingEDT(@NotNull @NonNls String name, @NotNull Condition<?> shutUpCondition,
+                   int maxUnitOfWorkThresholdMs, @NotNull Runnable pingAction) {
+        myName = name;
+        myShutUpCondition = shutUpCondition;
+        myMaxUnitOfWorkThresholdMs = maxUnitOfWorkThresholdMs;
+        this.pingAction = pingAction;
+    }
+
+    private boolean isEmpty() {
+        return !pinged;
+    }
+
+    private boolean processNext() {
+        pinged = false;
+        pingAction.run();
+        return pinged;
+    }
+
+    // returns true if invokeLater was called
+    public boolean ping() {
+        pinged = true;
+        return scheduleUpdate();
+    }
+
+    // returns true if invokeLater was called
+    private boolean scheduleUpdate() {
+        if (!stopped && invokeLaterScheduled.compareAndSet(false, true)) {
+            SwingUtilities.invokeLater(myUpdateRunnable);
+            return true;
+        }
+        return false;
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+}
